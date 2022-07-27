45f63951cdf23e8ab02a5685c2abdad88285f5bd
alekseevgr <alekseevgr2@gmail.com>
2022-07-27 12:11:06 +0500

    2

---
+++ bin/brain-even.js
@@ -0,0 +1,15 @@
+import readlineSync from 'readline-sync';
+import { name } from '../src/cli.js';
+const randomNumber = Math.round(Math.random * 100);
+console.log(`Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ${randomNumber}`);
+const answer = readlineSync.question('Your answer: ');
+const numberIsEven = (randomNumber) => {
+    return randomNumber % 2 === 0 ? 'yes' : 'no';
+};
+const even = (answer, numberIsEven) => {
+    if (answer === numberIsEven) {
+        console.log('Correct!')
+    };
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${numberIsEven}'.\nLet's try again, ${name}`)
+};
+export default even
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 import startGame from '../src/cli.js';
+import even from './brain-even.js';
 startGame();
-
+even
