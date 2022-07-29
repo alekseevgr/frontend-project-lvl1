aebe2c688dd8864f52405cf4cc229b3520be8a48
alekseevgr <alekseevgr2@gmail.com>
2022-07-29 09:39:20 +0500

    1p++

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,16 +3,14 @@ import { name } from '../src/cli.js';
 const randomNumber = Math.round(Math.random() * 100);
 console.log(`Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ${randomNumber}`);
 const answer = readlineSync.question('Your answer: ');
-let correctAnswer = '';
-const numberIsEven = () => {
-    return correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
-};
-const even = (answer, numberIsEven) => {
-    if (answer !== numberIsEven) {
-       return console.log('Correct!') 
+const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
+const win = 'Correct!';
+const lose = `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}`;
+const even = (answer, correctAnswer) => {
+    if (answer !== correctAnswer) {
+       console.log(win)
     } 
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}`);
-   
+    console.log(lose)
 };
 
 export default even
