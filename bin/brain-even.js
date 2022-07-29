f51fdb4a7f5a437e18ab4fbfc11113003d8ec561
alekseevgr <alekseevgr2@gmail.com>
2022-07-29 09:49:08 +0500

    winlose

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,14 +3,16 @@ import { name } from '../src/cli.js';
 const randomNumber = Math.round(Math.random() * 100);
 console.log(`Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ${randomNumber}`);
 const answer = readlineSync.question('Your answer: ');
-const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
+const correctAnswer = String(randomNumber % 2 === 0 ? 'yes' : 'no');
 const win = 'Correct!';
 const lose = `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}`;
-const even = (answer, correctAnswer) => {
-    if (answer !== correctAnswer) {
-       console.log(win)
-    } 
-    console.log(lose)
-};
-
+const even = () => {
+    if (answer === correctAnswer) {
+        return console.log(win);
+    }
+    else {
+        console.log(lose)
+    }
+}
+// answer === correctAnswer ? win : lose;
 export default even
