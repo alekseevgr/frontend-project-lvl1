f2867c72bebb861cf2e8d8413be920d2cdf48833
alekseevgr <alekseevgr2@gmail.com>
2022-07-30 13:50:10 +0500

    correct brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,18 +1,23 @@
 import readlineSync from 'readline-sync';
-import { name } from './brain-games.js'
+import name from './brain-games.js'
+const champion = `Congratulations, ${name}`;
+const brainEven = () => {
+    for (let i = 0; i <= 3; i = i + 1){
+        if ( i === 3) {
+            return console.log(champion)
+        }
 const randomNumber = Math.round(Math.random() * 100);
 console.log(`Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ${randomNumber}`);
 const answer = readlineSync.question('Your answer: ');
 const correctAnswer = String(randomNumber % 2 === 0 ? 'yes' : 'no');
 const win = 'Correct!';
 const lose = `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}`;
-const champion = `Congratulations, ${name}`;
-export const even = () => {
-    if (answer === correctAnswer) {
-        console.log(win);
-    }
-    else {
-        console.log(lose)
-    }
-  };
+if (answer === correctAnswer) {
+    console.log(win);
+} else {
+    return console.log(lose);
+};
+};
+};
+brainEven();
 // answer === correctAnswer ? win : lose
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import { even } from './brain-even';
 console.log('Welcome to the Brain Games!')
-export const name = readlineSync.question('May I have you name?');
+const name = readlineSync.question('May I have you name?');
 console.log(`Hello, ${name}!`)
-
-even
+export default name
  
