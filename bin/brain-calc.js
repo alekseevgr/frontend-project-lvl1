bcb5305306c5283a42f596384cdadaf76e23738e
alekseevgr <alekseevgr2@gmail.com>
2022-08-01 09:40:45 +0500

    switch even

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-import {
-    cons, car, cdr
-  } from '@hexlet/pairs';
+import { car, cdr, cons } from '@hexlet/pairs';
 import _ from 'lodash';
 import readlineSync from 'readline-sync';
 import name from './brain-games.js'
@@ -11,23 +9,22 @@ const brainCalc = () => {
         if ( i === 3) {
             return console.log(champion)
         }
-const randomNumber = Math.round(Math.random() * 10);
+const randomNumber = _.random(10);
 const pair = cons(randomNumber, randomNumber);
 const expressionSymbol = ['+', '-', '*'];
-const symbol = _.sample(expressionSymbol)
-console.log(`What is the result of the expression?.\nQuestion: ${car(pair)} ${symbol} ${cdr(pair)}`);
-const answer = readlineSync.question('Your answer: ');
+const symbol = expressionSymbol[_.random(expressionSymbol.length -1)]
+console.log(`What is the result of the expression?.\nQuestion: ${randomNumber} ${symbol} ${randomNumber}`);
+const answer = Number(readlineSync.question('Your answer: '));
 const correctAnswer = () => {
-    let result = 0;
-    if (symbol === '+'){
-        result = car(pair) + cdr(pair);
-    } else if (symbol === '-') {
-        result = car(pair) - cdr(pair);
-    } else if (symbol === '*') {
-        result = car(pair) * cdr(pair);
-    }
-    return result;
-};
+    switch (symbol) {
+        case '+':
+            return car(pair) + cdr(pair);
+        case '-':
+            return car(pair) - cdr(pair);
+        default:
+            return car(pair) * cdr(pair);
+    };
+}; 
 const win = 'Correct!';
 const lose = `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}`;
 if (answer === correctAnswer) {
