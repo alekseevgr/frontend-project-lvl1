c58b28bd69d98089eaa1eb00450a36b8135824fa
alekseevgr <alekseevgr2@gmail.com>
2022-08-02 17:23:05 +0500

    even.js index.js correct

--- src/games/even.js
+++ src/games/even.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 import startGame from '../index.js'
 const evenGame = () => {
-const randomNumber =  _.random(100);
 const rules = `Answer "yes" if the number is even, otherwise answer "no"`;
-const isEven = randomNumber % 2 === 0 ? 'yes' : 'no';
 const round = () => {
+    const randomNumber =  _.random(100);
+    const isEven = randomNumber % 2 === 0 ? 'yes' : 'no';
     const question = randomNumber;
     const correctAnswer = isEven;
 
