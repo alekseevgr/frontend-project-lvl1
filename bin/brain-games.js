5b298cf3f73dccadc6e65c2fc6318e68887f9a56
alekseevgr <alekseevgr2@gmail.com>
2022-08-04 10:55:30 +0500

    test 5

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,4 +3,4 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!')
 const name = readlineSync.question('May I have you name?');
 console.log(`Hello, ${name}!`)
- 
+export default name
