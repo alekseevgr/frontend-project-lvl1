1901b962102b319e4f39dfc703d0784d0ddfe105
alekseevgr <alekseevgr2@gmail.com>
2022-08-04 09:08:26 +0500

    progression game is correct

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,18 +5,28 @@ const progGame = () => {
     const round = () => {
         const numberCount = _.random(5, 10);
         let randomNumber = _.random(100);
-        const step = _.random(5);
+        const step = _.random(1, 5);
             let i = 1
             let result = [];
             while (i <= numberCount) {
                 result.push(randomNumber)
                 randomNumber = randomNumber + step;
                 i = i + 1;
-            }
-            const correctAnswer = result[_.random(numberCount)];
+            };
             result[_.random(numberCount)] = '..';
+            const correctAnswer = () => {
+                let correct;
+                for (let i = 0; i < result.length; i += 1) {
+                    if (i === 0 && (result[i] === '..')) {
+                        correct = String(result[i + 1] - step)
+                    }   else if (result[i] === '..') {
+                        correct = String(result[i - 1] + step);
+                    }
+                }
+            return correct;
+            }   
             const question = result;
-            return [question, correctAnswer];
+            return [question, correctAnswer()];
     };
 startGame(round, rules);
 };
