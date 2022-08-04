cc1686bd06b72b8bb2a7424e8916aca7a6a09224
alekseevgr <alekseevgr2@gmail.com>
2022-08-04 11:04:32 +0500

    test proggame

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,25 +6,21 @@ const progGame = () => {
         const numberCount = _.random(5, 10);
         let randomNumber = _.random(100);
         const step = _.random(1, 5);
-            let i = 1
-            let result = [];
-            while (i <= numberCount) {
-                result.push(randomNumber)
-                randomNumber = randomNumber + step;
-                i = i + 1;
-            };
-            result[_.random(numberCount)] = '..';
-            const correctAnswer = () => {
-                let correct;
-                for (let i = 0; i < result.length; i += 1) {
-                    if (i === 0 && (result[i] === '..')) {
-                        correct = String(result[i + 1] - step)
-                    }   else if (result[i] === '..') {
-                        correct = String(result[i - 1] + step);
-                    }
-                };
-            return correct;
-            };
+        let i = 1
+        let result = [];
+        while (i <= numberCount) {
+            result.push(randomNumber)
+            randomNumber = randomNumber + step;
+            i = i + 1;};
+        result[_.random(numberCount)] = '..';
+        const correctAnswer = () => {
+        let correct;
+        for (let i = 0; i < result.length; i += 1) {
+            if (i === 0 && (result[i] === '..')) {
+                correct = String(result[i + 1] - step)
+                }   else if (result[i] === '..') {
+                correct = String(result[i - 1] + step);}};
+            return correct;};
             const question = result;
             return [question, correctAnswer()];
     };
