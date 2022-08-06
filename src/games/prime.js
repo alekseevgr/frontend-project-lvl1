65d3414b447eccde9c12b196c471256105d3f11b
alekseevgr <alekseevgr2@gmail.com>
2022-08-06 19:49:16 +0500

    prime v4

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,16 +3,13 @@ import startGame from '../index.js';
 
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const prime = (num) => {
-  switch (num) {
-    case 0:
-    case 1:
+  if (num < 2) {
+    return 'no';
+  }
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
       return 'no';
-    default:
-      for (let i = 2; i < num; i += 1) {
-        if (num % i === 0) {
-          return 'no';
-        }
-      }
+    }
   }
   return 'yes';
 };
