72d5a55e7d5427bcff7ddabed76eda6c304a9597
alekseevgr <alekseevgr2@gmail.com>
2022-08-06 15:23:29 +0500

    remove lodash and pairs

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,12 +1,17 @@
 import getRandomInt from '../functionrandom.js';
 import startGame from '../index.js';
-
+// константа с правилами,
+//функция с главной логикой,
+//генератор раундов,
+//функция запуска игры, по сути просто передача генератора раундов и правил в основную логику, 
+// она уже есть сейчас, просто из нее лучше вынести правила и генератор раундов в корень файла.
 const calcGame = () => {
   const rules = 'What is the result of the expression?.';
+  const expressionSymbol = ['+', '-', '*'];
+  
   const round = () => {
     const randomNumber = getRandomInt(0, 100);
     const secondRandomNumber = getRandomInt(0, 100);
-    const expressionSymbol = ['+', '-', '*'];
     const symbol = expressionSymbol[getRandomInt(0, (expressionSymbol.length))];
 
     const question = `${randomNumber} ${symbol} ${secondRandomNumber}`;
