13d3e0d47e77097ba234d605b3e156d6f0148cf9
alekseevgr <alekseevgr2@gmail.com>
2022-08-11 16:03:05 +0500

    delete answer progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,18 +2,6 @@ import getRandomInt from '../getRandomInt.js';
 import startGame from '../index.js';
 
 const rules = 'What number is missing in the progression?';
-
-const answer = (progression, step) => {
-  let correct;
-  for (let i = 0; i < progression.length; i += 1) {
-    if (i === 0 && (progression[i] === '..')) {
-      correct = progression[i + 1] - step;
-    } else if (progression[i] === '..') {
-      correct = progression[i - 1] + step;
-    }
-  }
-  return correct;
-};
 const generateProgression = (num, step, numberCount) => {
   const array = [];
   let j = 1;
@@ -30,9 +18,9 @@ const generateRound = () => {
   const step = getRandomInt(1, 5);
   const numberCount = getRandomInt(5, 10);
   const progression = generateProgression(num, step, numberCount);
-
-  progression[getRandomInt(0, numberCount)] = '..';
-  const correctAnswer = String(answer(progression, step));
+  const hiddenNumberIndex = getRandomInt(0, numberCount);
+  progression[hiddenNumberIndex] = '..';
+  const correctAnswer = String(num + hiddenNumberIndex * step);
   const question = progression.join(' ');
   return [question, correctAnswer];
 };
